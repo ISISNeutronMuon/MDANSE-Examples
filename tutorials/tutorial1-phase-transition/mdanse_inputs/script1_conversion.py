4b0d4e7103c57ac19fb3fa08320deb4bea0d98e4
Chi Cheng <chicheng85@gmail.com>
2024-08-09 09:25:54 +0100

    updated tutorial scripts so it works with the latest version of MDANSE

--- tutorials/tutorial1-phase-transition/mdanse_inputs/script1_conversion.py
+++ tutorials/tutorial1-phase-transition/mdanse_inputs/script1_conversion.py
@@ -16,7 +16,7 @@ parameters = {
     'fold': False,                                      # Fold coordinates in to box
     'lammps_units': 'electron',                         # LAMMPS unit system
     'n_steps': '0',                                     # Number of time steps (0 for automatic detection)
-    'output_file': ('../mdanse_outputs/converted_trajectory', 32, 'gzip'),  # MDANSE trajectory (filename, format)
+    'output_files': ('../mdanse_outputs/converted_trajectory', 32, 'gzip', "INFO"),  # MDANSE trajectory (filename, format)
     'time_step': '0.2',                                 # Time step (lammps units, depends on unit system)
     'trajectory_file': '../md_outputs/trajectory.txt',  # LAMMPS trajectory file
     'trajectory_format': 'custom',                      # LAMMPS trajectory format
