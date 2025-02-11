4eb09679ee1842258c5413864b380d84ced5ce0c
Chi Cheng <chicheng85@gmail.com>
2025-02-11 11:37:49 +0000

    fixed tutorial 3 scripts

--- tutorials/tutorial3-quasielastic-neutron-scattering/mdanse_inputs/script3_gdisf.py
+++ tutorials/tutorial3-quasielastic-neutron-scattering/mdanse_inputs/script3_gdisf.py
@@ -16,7 +16,7 @@ parameters = {
     'frames': [0, 1001, 1, 501],                        # frames
     'grouping_level': 'atom',                           # grouping_level
     'instrument_resolution': ('ideal', {}),             # instrument_resolution
-    'output_files': ('../mdanse_outputs/gaussiandynamicincoherentstructurefactor.mda', ['MDAFormat'], 'no logs'),  # output_files
+    'output_files': ('../mdanse_outputs/gaussiandynamicincoherentstructurefactor.mda', ['MDAFormat'], 'INFO'),  # output_files
     'projection': ('NullProjector', []),                # project coordinates
     'q_shells': [10.0, 44.0, 2.0],                      # q_shells
     'running_mode': ('single-core',),                   # running_mode
