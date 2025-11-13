1a7e00a11058df1d8774395b42003b7a566db46a
chi cheng <chi-yang.cheng@proton.me>
2025-11-13 15:00:27 +0000

    update tutorial 3

--- tutorials/tutorial3-quasielastic-neutron-scattering/mdanse_inputs/script3_gdisf.py
+++ tutorials/tutorial3-quasielastic-neutron-scattering/mdanse_inputs/script3_gdisf.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python
+#!C:\Users\xcb63893\AppData\Local\miniconda3\envs\MDANSE\python.exe
+
+import os
+
+os.environ.update(
+    OMP_NUM_THREADS="1",
+    OPENBLAS_NUM_THREADS="1",
+    MKL_NUM_THREADS="1",
+    VECLIB_MAXIMUM_THREADS="1",
+    NUMEXPR_NUM_THREADS="1",
+)
 
 ########################################################
 # This is an automatically generated MDANSE run script #
@@ -11,16 +21,20 @@ from MDANSE.Framework.Jobs.IJob import IJob
 ########################################################
 
 parameters = {
-    'atom_transmutation': '{}',                         # atom_transmutation
-    'frames': [0, 1001, 1, 501],                        # frames
-    'grouping_level': 'atom',                           # grouping_level
-    'instrument_resolution': ('ideal', {}),             # instrument_resolution
-    'output_files': ('../mdanse_outputs/gaussiandynamicincoherentstructurefactor.mda', ['MDAFormat'], 'INFO'),  # output_files
-    'projection': ('NullProjector', []),                # project coordinates
-    'q_shells': [10.0, 44.0, 2.0],                      # q_shells
-    'running_mode': ('single-core',),                   # running_mode
-    'trajectory': '../mdanse_outputs/converted_trajectory.mdt',  # trajectory
-    'weights': 'equal',                                 # weights
+    "atom_transmutation": "{}",  # atom_transmutation
+    "frames": [0, 1001, 1, 501],  # frames
+    "grouping_level": "atom",  # grouping_level
+    "instrument_resolution": ("ideal", {}),  # instrument_resolution
+    "output_files": (
+        "../mdanse_outputs/gaussiandynamicincoherentstructurefactor.mda",
+        ["MDAFormat"],
+        "INFO",
+    ),  # output_files
+    "projection": ("NullProjector", []),  # project coordinates
+    "q_shells": [10.0, 44.0, 2.0],  # q_shells
+    "running_mode": ("single-core",),  # running_mode
+    "trajectory": "../mdanse_outputs/converted_trajectory.mdt",  # trajectory
+    "weights": "equal",  # weights
 }
 
 ########################################################
@@ -28,5 +42,9 @@ parameters = {
 ########################################################
 
 if __name__ == "__main__":
-    gaussiandynamicincoherentstructurefactor = IJob.create('GaussianDynamicIncoherentStructureFactor')
-    gaussiandynamicincoherentstructurefactor.run(parameters, status=True)
+    gaussiandynamicincoherentstructurefactor = IJob.create(
+        "GaussianDynamicIncoherentStructureFactor"
+    )
+    # Progress bars only available if tqdm available.
+    # Install with `cli` optional dependency.
+    gaussiandynamicincoherentstructurefactor.run(parameters, status=True, prog_bar=True)
